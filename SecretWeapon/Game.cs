92f58df971dbb3234c44b73b41b2dfd9b235d5f2
George Santa <george.santa@tba.group>
2022-02-18 18:08:49 +0200

    Tidy up and new homework

---
+++ SecretWeapon/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretWeapon
+{
+    internal class Game
+    {
+
+        public int x;
+        public int y;
+
+
+        public Game(int difficulty)
+        {
+            Random random = new Random();
+            x = Convert.ToInt32(random.NextDouble() * difficulty + 1);
+            y = Convert.ToInt32(random.NextDouble() * difficulty + 1);
+        }
+
+        public double Fire(int x, int y)
+        {
+            double z = Math.Sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+
+            return z;
+        }
+    }
+}
--- SecretWeapon/Program.cs
+++ SecretWeapon/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 
 namespace SecretWeapon
 {
-    //store scores in a class with player names
-    // TO DO: 
+    // Take a look into Domain Driven Design!
+    // TO DO: Change the game to be able to play with as many as specified!
+    // TO DO: Lookup serialization and safe scores for every player to display a high score table.
     public class Program
     {
         static void Main(string[] args)
@@ -12,23 +13,23 @@ namespace SecretWeapon
             bool newRound = true;
             while (newRound)
             {
-                Player firstPlayer = new Player();
-                Player secondPlayer = new Player();
 
+                Player player1 = new Player();
+                Player player2 = new Player();
                 Console.WriteLine("SECRET WEAPON \n");
                 Console.WriteLine("Only 2 players. \n");
                 Console.WriteLine("ENTER FIRST PLAYERS NAME \n");
                 string name = Console.ReadLine();
-                firstPlayer.UpdateScoreAndName(name, 0);
+                player1.UpdateScoreAndName(name, 0);
 
                 Console.WriteLine("ENTER SECOND PLAYERS NAME\n");
                 name = Console.ReadLine();
-                secondPlayer.UpdateScoreAndName(name, 0);
+                player2.UpdateScoreAndName(name, 0);
 
                 int difficulty = 0;
                 List<Player> playerList = new List<Player>();
-                playerList.Add(firstPlayer);
-                playerList.Add(secondPlayer);
+                playerList.Add(player1);
+                playerList.Add(player2);
                 
                 while (difficulty < 4)
                 {
@@ -36,9 +37,8 @@ namespace SecretWeapon
                     int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out difficulty);
                 }
 
-                Random random = new Random();
-                int x = Convert.ToInt32(random.NextDouble() * difficulty + 1);
-                int y = Convert.ToInt32(random.NextDouble() * difficulty + 1);
+                var game = new Game(difficulty);
+
                 int loop = 1;
                 bool victory = false;
 
@@ -48,15 +48,7 @@ namespace SecretWeapon
 
                     foreach (Player player in playerList)
                     {
-
-                        if (player.PlayerName == secondPlayer.PlayerName)
-                        {
-                            Console.WriteLine($"{player.PlayerName} is your turn! \n");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{firstPlayer.PlayerName} your turn! \n");
-                        }
+                        Console.WriteLine($"{player.PlayerName} is your turn! \n");
 
                         ClearKeyPresses();
                         Console.WriteLine("Guesses for X and Y");
@@ -69,13 +61,12 @@ namespace SecretWeapon
                         y1 = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine($"Y1 selected = {y1}");
 
-                        double z = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+                        var z = game.Fire(x1, y1);
 
                         if (z == 0)
                         {
                             victory = SuccessfulyDestroyed(loop, player.PlayerName);
                             player.PlayerScore += 10;
-                            player.DisplayScore();
                             break;
 
                         }
@@ -83,12 +74,13 @@ namespace SecretWeapon
                         {
                             Console.WriteLine("Close\n");
                             player.PlayerScore += 1;
-                            player.DisplayScore();
                         }
                         else
                         {
                             Console.WriteLine("Not even close\n");
                         }
+
+                        player.DisplayScore();
                     }
                     if (victory)
                     {
@@ -96,14 +88,14 @@ namespace SecretWeapon
                     }
                     loop++;
                 }
-                if (firstPlayer.PlayerScore < (difficulty + 5) && secondPlayer.PlayerScore < (difficulty + 5))
+                if (player1.PlayerScore < (difficulty + 5) && player2.PlayerScore < (difficulty + 5))
                 {
                     Console.WriteLine("THE ROBOTS HAVE SEEN YOU - AGGGHHHHH....");
                     Console.Read();
                 }
 
                 CalculateWinner(playerList);
-                newRound = PlayNewRound() ? true : false;
+                newRound = PlayNewRound();
             }
             
         }
@@ -140,11 +132,9 @@ namespace SecretWeapon
 
         public static bool PlayNewRound()
         {
-            bool newRound = true;
             Console.WriteLine("WANT TO PLAY A NEW ROUND?");
 
-            newRound = Console.ReadLine().ToString() == "y" ? newRound : false;
-            return newRound;
+            return Console.ReadLine().ToString() == "y";
         }
     }
 }
